347716f764551496d3c070e64d198173eb73a5b3
mumushuiding <181457121@qq.com>
2019-11-21 11:23:58 +0800

    基本框架搭建

---
+++ router/router.go
@@ -0,0 +1,30 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/codepository/GoWebAnalytics/config"
+	"github.com/codepository/GoWebAnalytics/controller"
+)
+
+// Mux 路由
+var Mux = http.NewServeMux()
+var conf = *config.Config
+
+func interceptor(h http.HandlerFunc) http.HandlerFunc {
+	return crossOrigin(h)
+}
+func crossOrigin(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", conf.AccessControlAllowOrigin)
+		w.Header().Set("Access-Control-Allow-Methods", conf.AccessControlAllowMethods)
+		w.Header().Set("Access-Control-Allow-Headers", conf.AccessControlAllowHeaders)
+		h(w, r)
+	}
+}
+func init() {
+	setMux()
+}
+func setMux() {
+	Mux.HandleFunc("/api/v1/test/index", interceptor(controller.Index))
+}
